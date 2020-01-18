03a29de28e5054f77fbe3377363d62414c47be43
victrid <weihao.jiang@sjtu.edu.cn>
2020-01-18 18:19:09 +0800

    upload Jan-18

--- leetcode/l0007.cpp
+++ leetcode/l0007.cpp
@@ -1,20 +1,27 @@
 //that's ugly.
 //using some ugly edge configures
 
-class Solution {
+class Solution
+{
 public:
-    int reverse(int x) {
+    int reverse(int x)
+    {
         int ret = 0;
-        bool retp = x>=0;
-        if(x == -2147483648)return 0;
-        if(!retp)x=-x;
-        while(x!=0){
-            if(ret>214748364)return 0;bian
-            ret *= 10;
-            if(ret==214748364&&x%10>7)return 0;
-            ret += x%10;
+        bool retp = x >= 0;
+        if (x == -2147483648)
+            return 0;
+        if (!retp)
+            x = -x;
+        while (x != 0)
+        {
+            if (ret > 214748364)
+                return 0;
+                ret *= 10;
+            if (ret == 214748364 && x % 10 > 7)
+                return 0;
+            ret += x % 10;
             x /= 10;
         }
-        return retp?ret:-ret;
+        return retp ? ret : -ret;
     }
 };
---
+++ leetcode/l0344.cpp
@@ -0,0 +1,10 @@
+void reverseString(char *s, int sSize)
+{
+    char temp;
+    for (int i = 1; i <= sSize / 2; i++)
+    {
+        temp = s[i - 1];
+        s[i - 1] = s[sSize - i];
+        s[sSize - i] = temp;
+    }
+}
