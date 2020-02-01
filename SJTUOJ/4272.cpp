f5ac749d0f78476be8bbdc56ecbbb82ae10521f3
victrid <weihao.jiang@sjtu.edu.cn>
2020-02-01 09:49:12 +0800

    M:4272:delete untouchable conditions

--- SJTUOJ/4272.cpp
+++ SJTUOJ/4272.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-bool isMarvel(int number)
-{
+bool isMarvel(int number) {
     while (number % 2 == 0)
         number /= 2;
     while (number % 3 == 0)
@@ -15,22 +14,19 @@ bool isMarvel(int number)
     return number == 1;
 }
 
-int main()
-{
+int main() {
     int Minimum, Maximum;
     cin >> Minimum >> Maximum;
-    //error leads to quit
-    if (Minimum < 1 || Minimum > 99999 || Maximum < 1 || Maximum > 99999 || Minimum > Maximum)
-    {
+    // error leads to quit
+    if (Minimum < 1 || Minimum > 99999 ||
+        Minimum > Maximum) {
         cout << "error";
         return 0;
     }
-    //go through limits
+    // go through limits
     bool FOUND_FLAG = false;
-    for (int i = Minimum; i <= Maximum; i++)
-    {
-        if (isMarvel(i))
-        {
+    for (int i = Minimum; i <= Maximum; i++) {
+        if (isMarvel(i)) {
             if (FOUND_FLAG)
                 cout << endl;
             FOUND_FLAG = true;
