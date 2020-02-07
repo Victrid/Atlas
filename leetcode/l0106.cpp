fc862c0c71353cf07425c160885d56f8e5d1e0e3
VictriD <weihao.jiang@sjtu.edu.cn>
2020-02-07 17:49:59 +0800

    A:l106:same as 105

---
+++ leetcode/l0106.cpp
@@ -0,0 +1,30 @@
+#include "lc.hpp"
+class Solution {
+public:
+vector<int>::iterator find(vector<int>::iterator begin, vector<int>::iterator end, int num) {
+        for (vector<int>::iterator f = begin; f < end; f++)
+            if (*f == num)
+                return f;
+        return end;
+    }
+    TreeNode* buildTree(vector<int>::iterator pbegin, vector<int>::iterator pend, vector<int>::iterator ibegin, vector<int>::iterator iend) {
+        if (pbegin == pend)
+            return nullptr;
+        int root                          = *(pend-1);
+        TreeNode* TreeRoot                = new TreeNode(root);
+        vector<int>::iterator rootinorder = find(ibegin, iend, root);
+        int leftsize                      = rootinorder - ibegin;
+        TreeRoot->left                    = buildTree(pbegin, pbegin +leftsize, ibegin, ibegin + leftsize);
+        TreeRoot->right                   = buildTree(pbegin + leftsize, pend-1, ibegin + 1 + leftsize, iend);
+        return TreeRoot;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return buildTree(postorder.begin(), postorder.end(), inorder.begin(), inorder.end());
+    }
+};
+int main(){
+    Solution sol;
+    vector<int> a={9,3,15,20,7},b={9,15,7,20,3};
+    sol.buildTree(a,b);
+    return 0;
+}
