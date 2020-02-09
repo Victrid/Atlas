24801594c5c574f66ef21a47d935a628841f0c02
VictriD <weihao.jiang@sjtu.edu.cn>
2020-02-09 12:28:37 +0800

    M:l5332:brute-force.[暴力法:我太弱了[快哭了][快哭了]]

--- leetcode/l5332.cpp
+++ leetcode/l5332.cpp
@@ -1,38 +1,13 @@
-//!wrong
 #include "lc.hpp"
+
 class Solution {
 public:
-    bool find(vector<int>::iterator begin, vector<int>::iterator end, double s) {
-        if (end - begin == 0)
-            return false;
-        if (*begin > s || *end < s)
-            return false;
-        if (*begin == s || *end == s)
-            return true;
-
-        auto mid = begin + (end - begin) / 2;
-        return find(begin, mid, s) || find(mid + 1, end, s);
-    }
     bool checkIfExist(vector<int>& arr) {
-        if (arr.begin() == arr.end())
-            return false;
-        sort(arr.begin(), arr.end());
-        for (auto i = arr.end() - 1; i > arr.begin(); i--) {
-            int z = *i;
-            i     = arr.erase(i);
-            if (z > 0 && find(arr.begin(), arr.end()-1, z / 2.0))
-                return true;
-            if (z == 0 && find(arr.begin(), arr.end()-1, 0))
-                return true;
-            if (z < 0 && find(arr.begin(), arr.end()-1, z * 2))
-                return true;
-        }
+        int l = arr.size();
+        for (int i = 0; i < l; i++)
+            for (int j = i + 1; j < l; j++)
+                if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
+                    return true;
         return false;
     }
 };
-int main() {
-    vector<int> a = {0,0};
-    Solution S;
-    S.checkIfExist(a);
-    return 0;
-}
